ab31361d35b4a8f5bbcacfc0ec75b735c382fc36
Kian Noctor <kian.noctor@universalstore.com.au>
2020-12-15 19:57:14 +1000

    Current WIP

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+//Import Page Components
+import AboutSection from '../components/AboutSection';
+import ServicesSection from '../components/ServicesSection';
+import FaqSection from '../components/FaqSection';
+
+//Animations
+import { motion } from 'framer-motion';
+import { pageAnimation } from '../animation';
+const AboutUs = () => {
+    return (
+        <motion.div exit="exit" variants={pageAnimation} initial='hidden' animate='show'>
+        <AboutSection />
+        <ServicesSection />
+        <FaqSection />
+        </motion.div>
+    )
+}
+
+export default AboutUs;
---
+++ src/pages/OurWork.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+//Images
+import athlete from '../img/athlete-small.png';
+import theracer from '../img/theracer-small.png';
+import goodtimes from '../img/goodtimes-small.png';
+
+//Animations
+import { motion } from 'framer-motion';
+import { pageAnimation, fade, photoAnim } from '../animation';
+const OurWork = () => {
+    return(
+        <Work 
+            exit="exit" 
+            variants={pageAnimation} 
+            initial='hidden' 
+            animate='show'
+            style={{background: '#fff'}}
+        >
+            <Movie>
+                <motion.h2 variants={fade}>The Athlete</motion.h2>
+                <motion.div className="line"></motion.div>
+                <Link to="/work/the-athlete">
+                    <Hide>
+                        <motion.img variants={photoAnim} src={athlete} alt="athlete" />
+                    </Hide>
+                </Link>
+            </Movie>
+            <Movie>
+                <h2>The Racer</h2>
+                <div className="line"></div>
+                <Link to="/work/the-racer">
+                    <img src={theracer} alt="the racer" />
+                </Link>
+            </Movie>
+            <Movie>
+                <h2>Good Times</h2>
+                <div className="line"></div>
+                <Link to="/work/good-times">
+                    <img src={goodtimes} alt="goodtimes" />
+                </Link>
+            </Movie>
+        </Work>
+    )
+}
+
+const Work = styled(motion.div)`
+    min-height: 100vh;
+    overflow: hidden;
+    padding: 5rem 10rem;
+
+    h2{
+        padding: 1rem 0rem;
+    }
+`
+const Movie = styled.div`
+    padding-bottom: 10rem;
+
+    .line {
+        height: 0.5rem;
+        background: #23d997;
+        margin-bottom: 3rem;
+    }
+
+    img {
+        width: 100%;
+        height: 70vh;
+        object-fit: cover;
+    }
+`
+
+const Hide = styled.div`
+    overflow: hidden;
+`
+
+export default OurWork;
